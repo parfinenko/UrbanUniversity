16bd1de9914c0587fdcc3059f8245601907b9cef
Roman <145841256+parfinenko@users.noreply.github.com>
2024-09-04 22:19:31 +0600

    Переменные

---
+++ Lesson3.py
@@ -0,0 +1,5 @@
+amount_of_homework_completed = 12
+number_of_hours_spent = 1.5
+course_title = "Python"
+time_per_task = (amount_of_homework_completed / (number_of_hours_spent * 60 ) )
+print(f"Курс:{course_title}, всего задач:{amount_of_homework_completed}, затрачено часов:{number_of_hours_spent}, среднее время выполнения {time_per_task} часа.")
