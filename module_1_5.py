c7d03cc8262babe0db0cde96da0aed6d6aeacf94
Roman <145841256+parfinenko@users.noreply.github.com>
2024-09-05 15:41:55 +0600

    Неизменяемые и изменяемые объекты. Кортежи

---
+++ module_1_5.py
@@ -0,0 +1,9 @@
+immutable_var = tuple(['1','erwer','1.0','true'])
+print(immutable_var)
+
+#immutable_var [0] = 3 невозможно присвоить новое значение
+
+mutable_list = ["apple", "0", "true", "123213.232131"]
+print(mutable_list)
+mutable_list [0] = 444
+print(mutable_list)
