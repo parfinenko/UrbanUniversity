0fbb8f9d8708aa52ef3408699be9d7af84c54eaa
Roman <145841256+parfinenko@users.noreply.github.com>
2025-01-09 05:14:04 +0600

    Различие атрибутов класса и экземпляра.

--- module_5_1.py
+++ module_5_1.py
@@ -3,7 +3,6 @@ class House:
         self.name = name
         self.number_or_floors = number_or_floors
 
-
     def go_to(self, new_floor):
         if new_floor < 1 or new_floor > self.number_or_floors:
             print('Такого этажа не существует')
@@ -47,12 +46,17 @@ class House:
         self.number_or_floors += value
         return self
 
+    houses_history = []
 
+    def __new__(cls, *args, **kwargs):
+        instance = super(House, cls).__new__(cls)
+        house_name = args[0]
+        cls.houses_history.append(house_name)
+        return instance
 
+    def __init__(self, name, floors):
+        self.name = name
+        self.floors = floors
 
-
-
-# h1 = House('ЖК Горский', 18)
-# h2 = House('Домик в деревне', 2)
-# h1.go_to(5)
-# h2.go_to(10)
+    def __del__(self):
+        print(f"{self.name} снесён, но он останется в истории")
